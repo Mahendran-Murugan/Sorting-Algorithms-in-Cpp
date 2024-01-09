c53584d3fe16c240abd721fa325f736063a606ca
mahi000007 <mahendran1482004@gmail.com>
2024-01-09 21:32:49 +0530

    Merge Sort Added

---
+++ mergeSort.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merging(vector<int> &arr, int mid, int low, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] < arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSorting(vector<int> &arr, int low, int high)
+{
+    if (low == high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSorting(arr, low, mid);
+    mergeSorting(arr, mid + 1, high);
+    merging(arr, mid, low, high);
+}
+
+void mergeSort(vector<int> &arr, int n)
+{
+    mergeSorting(arr, 0, n - 1);
+}
+
+int main()
+{
+    vector<int> arr{9, 3, 2, 2, 1, 20, 0};
+    int n = arr.size();
+    mergeSort(arr, n);
+    cout << "The sorted Array after Merge Sort is: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
